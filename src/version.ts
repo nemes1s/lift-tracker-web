9ac90e4332a2fbbdb67acd6632f4aabe52e350cc
Dmytro Zadorozhnyi <dmitriyzadorozniy@gmail.com>
2025-11-01 21:59:50 +0100

    chore: bump version [skip ci]

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 // Auto-generated version file
 // This file is updated automatically by the version bump script
 
-export const APP_VERSION = '0.5.5';
-export const BUILD_DATE = '2025-11-01T20:31:55.320Z';
+export const APP_VERSION = '0.5.6';
+export const BUILD_DATE = '2025-11-01T20:59:50.401Z';
